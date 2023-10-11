9258351553c6f7d8a864f23d163c70bc2b735c8a
Bima42 <tanpau42@gmail.com>
2023-10-11 15:46:16 +0200

    feat(home): landing view done

---
+++ src/views/Home/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+	<section class="home-view">
+		<LandingElement/>
+	</section>
+</template>
+
+<script setup lang="ts">
+import LandingElement from '@/views/Home/elements/LandingElement.vue';
+</script>
+
+<style scoped lang="scss">
+.home-view {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	width: 100%;
+	height: 100%;
+	border-top: $primary-orange-light solid 1px;
+}
+</style>
---
+++ src/views/Home/elements/LandingElement.vue
@@ -0,0 +1,65 @@
+<template>
+	<div class="background-container">
+		<div class="content">
+			<p style="font-weight: bold">IaaS, PaaS, SaaS</p>
+			<h1 class="title">Vous méritez mieux que du greenwashing !</h1>
+			<p><span style="color: #F77B0B"><b>CloudAssess</b>, un <b>outil open source</b></span> pour évaluer automatiquement <b>votre empreinte environnementale</b> en temps que fournisseur de services cloud.</p>
+			<button @click="redirectDemo" aria-label="Réserver une démo">Réserver votre démo !</button>
+		</div>
+		<div class="image">
+			<img class="icons-background" src="@/assets/media/landing-icons-background.svg" alt="icons-background"/>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+const redirectDemo = () => window.location.href = 'https://calendly.com/resilio-tech/demo-cloudassess';
+</script>
+
+<style scoped lang="scss">
+.background-container {
+	background-image: url('@/assets/media/cloud-background.svg');
+	background-size: cover;
+	background-position: center;
+	background-repeat: no-repeat;
+
+	height: 100%;
+	display: flex;
+
+	.content {
+		display: flex;
+		flex-direction: column;
+		gap: $large-gap;
+		padding: $xlarge-padding $medium-padding;
+
+		h1 {
+			color: $primary-orange-light;
+		}
+
+		button {
+			background: $primary-orange-light;
+			width: 70%;
+			height: 30px;
+			align-self: center;
+			border: none;
+			border-radius: 15px;
+			font-weight: bold;
+			font-size: 14px;
+			color: white;
+		}
+	}
+
+	.image .icons-background {
+			width: 100%;
+	}
+}
+
+@media screen and (max-width: 768px) {
+	// Styles for mobile screens
+	.background-container {
+		background-position-y: 180px;
+		background-position-x: -560px;
+		flex-direction: column;
+	}
+}
+</style>
